10bd148f6dd83d186bf9577f81631f17d8a77c20
Steale <alexs-spam@arcor.de>
2016-07-04 17:17:27 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -2,18 +2,26 @@ var storageManager = {
 
 run: function(nameOfRoom) {
     //console.log(nameOfRoom);
-var avsources = Game.rooms[nameOfRoom].find(FIND_SOURCES);
-       var totalAmountOFEnergy = 0;
-       avsources.forEach(function(source){
-        totalAmountOFEnergy+= source.energyCapacity;
-        });
-       
-       var maxCargoEnergy = totalAmountOFEnergy/4;
-       //console.log("Max Cargo: ".concat(maxCargoEnergy));
-       //console.log("ENERGY AMOUNT ".concat(totalAmountOFEnergy));
-       
-       Game.rooms[nameOfRoom].memory.ress = totalAmountOFEnergy;
-       Game.rooms[nameOfRoom].memory.scanned = 'true';
+
+      
+      if (!Game.rooms[nameOfRoom].memory.scanned)  // wird nur ausgeführt wenn "scanned" == "false"
+      {
+            var avsources = Game.rooms[nameOfRoom].find(FIND_SOURCES);
+            var totalAmountOFEnergy = 0;
+            avsources.forEach(function(source){
+                totalAmountOFEnergy+= source.energyCapacity;
+            });
+            var maxCargoEnergy = totalAmountOFEnergy/4;
+            //console.log("Max Cargo: ".concat(maxCargoEnergy));
+            //console.log("ENERGY AMOUNT ".concat(totalAmountOFEnergy));
+            Game.rooms[nameOfRoom].memory.ress = totalAmountOFEnergy;
+            Game.rooms[nameOfRoom].memory.scanned = 'true';
+      }
+      
+      
+      // Entsheidung ob Storage gebaut werden muss
+      
+      
 }
 };
 
