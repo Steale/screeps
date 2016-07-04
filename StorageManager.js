00a6e5804a0e6084c48c5edd1f984d4ba63be449
Steale <alexs-spam@arcor.de>
2016-07-04 17:17:27 +0200

    updated upgrader and StorageManager

--- StorageManager.js
+++ StorageManager.js
@@ -1,14 +1,28 @@
-var Storage = require('RoomStorage');
-
-
 var storageManager = {
 
-    run: function(roomName) {
-        var room = roomName;
-        console.log("RoomName: ".concat(room));
-            Storage.run(room);
-    }
+run: function(nameOfRoom) {
+    //console.log(nameOfRoom);
 
+      
+      if (!Game.rooms[nameOfRoom].memory.scanned)  // wird nur ausgeführt wenn "scanned" == "false"
+      {
+            var avsources = Game.rooms[nameOfRoom].find(FIND_SOURCES);
+            var totalAmountOFEnergy = 0;
+            avsources.forEach(function(source){
+                totalAmountOFEnergy+= source.energyCapacity;
+            });
+            var maxCargoEnergy = totalAmountOFEnergy/4;
+            //console.log("Max Cargo: ".concat(maxCargoEnergy));
+            //console.log("ENERGY AMOUNT ".concat(totalAmountOFEnergy));
+            Game.rooms[nameOfRoom].memory.ress = totalAmountOFEnergy;
+            Game.rooms[nameOfRoom].memory.scanned = 'true';
+      }
+      
+      
+      // Entsheidung ob Storage gebaut werden muss
+      
+      
+}
 };
 
 module.exports = storageManager;
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var RoomController = require('RoomController');
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function() {
 
@@ -7,18 +10,46 @@ module.exports.loop = function() {
 		RoomController.run(name);
 	}
 
-    //CreepLoop
-	for ( var name in Game.creeps) {
-		var creep = Game.creeps[name];
-		if (creep.memory.role == 'harvester') {
-			roleHarvester.run(creep);
-		}
-		if (creep.memory.role == 'upgrader') {
-			roleUpgrader.run(creep);
-		}
-		if (creep.memory.role == 'builder') {
-			roleBuilder.run(creep);
-		}
-	}
+
+	  for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+for(var name in Game.rooms) {
+        if (Game.rooms[name].energyAvailable >= 300)
+        {
+            var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+            if(harvesters.length < 2) {
+                var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],'Sammler'+(harvesters.length+1).toString(), {role: 'harvester'});
+                console.log('Spawning new harvester: ' + newName);
+            }
+            
+            var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+            if(builder.length < 1 && harvesters.length > 1) {
+                var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],'Arbeiter'+(builder.length+1).toString(), {role: 'builder'});
+                console.log('Spawning new builder: ' + newName);
+            }    
+            var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+            if(upgrader.length < 1 && harvesters.length > 1) {
+                var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], 'Upgrader'+(upgrader.length+1).toString(), {role: 'upgrader'});
+            console.log('Spawning new upgrader: ' + newName);
+            }  
+        }
+}
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
 
 }
