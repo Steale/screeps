39da3b1b765639897ba8dffa88e9fc3ff0a7fef1
Steale <alexs-spam@arcor.de>
2016-07-04 21:55:48 +0200

    Update main.js
    Rangfolge für creep-Spawns festegelegt

--- main.js
+++ main.js
@@ -29,17 +29,19 @@ for(var name in Game.rooms) {
         if (Game.rooms[name].energyAvailable >= 300)
         {
             var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+            var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+            var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
             if(harvesters.length < 2) {
                 var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],'Sammler'+(harvesters.length+1).toString(), {role: 'harvester'});
                 console.log('Spawning new harvester: ' + newName);
             }
             
-            var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-            if(builder.length < 1 && harvesters.length > 1) {
+           
+            if(builder.length < 1 && harvesters.length > 1 && upgrader.length >0) {
                 var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],'Arbeiter'+(builder.length+1).toString(), {role: 'builder'});
                 console.log('Spawning new builder: ' + newName);
             }    
-            var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+           
             if(upgrader.length < 1 && harvesters.length > 1) {
                 var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], 'Upgrader'+(upgrader.length+1).toString(), {role: 'upgrader'});
             console.log('Spawning new upgrader: ' + newName);
