c670371dae76620812d1115f5ff3f05db39fd7e7
Steale <alexs-spam@arcor.de>
2016-07-04 17:02:38 +0200

    Update role.upgrader.js
    Upgrader Status hinzugefügt. Nun transportieren sie auch mehr als 1Energy

--- role.upgrader.js
+++ role.upgrader.js
@@ -3,14 +3,14 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
         
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
+         if(creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
 	    }
 	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.upgrading = true;
 	    }
         
-	    if(creep.memory.upgrading) {  
+        if(!creep.memory.upgrading) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
@@ -22,6 +22,8 @@ var roleUpgrader = {
             }
         }
 	}
+        
+       
 };
 
 module.exports = roleUpgrader;
